37de7efe5708014709898ed35d1908030dfb7a14
veeti-from <142967295+veeti-from@users.noreply.github.com>
2025-01-22 11:55:07 +0200

    fixes

---
+++ darksurvival/control.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+
+
+
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://start_screen.tscn")
+
+
+func _on_again_pressed() -> void:
+	get_tree().change_scene_to_file("res://game.tscn")
