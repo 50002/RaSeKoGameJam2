1d587cc8c2d21b07c923e22c845cb19bc2bbc500
50002 <142967542+50002@users.noreply.github.com>
2025-01-22 10:39:29 +0200

    bullet begin

--- darksurvival/agis.gd
+++ darksurvival/agis.gd
@@ -1,22 +1,21 @@
 extends CharacterBody2D
 
-var speed = 60
+var speed = 10
 var player_chase = false
 var player = null
 var health = 5
 var state = 0
 
-@onready var hitbox: CollisionShape2D = $attack/CollisionShape2D
+func _ready() -> void:
+	player = get_parent().get_node("CharacterBody2D")
+	
 
 func _physics_process(delta: float) -> void:
 	$AnimatedSprite2D.play("idle")
 	if state == 0:
-		if player_chase:
-			position.x += (player.position.x - position.x)/speed
+		position += (player.position - position)/speed
 			
 
 
-
-func _on_area_2d_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
-	player = body
-	player_chase = true
+func _on_attack_timeout() -> void:
+	$CollisionShape2D/FanAttack/bullet.rotate(player.position)
